032709bca7d00667bd014eab627139d63b04ecef
Matt Chennells <mchennells@gmail.com>
2023-04-10 14:08:38 +0100

    Delete extra bits

--- weather.R
+++
@@ -1,8 +0,0 @@
-library(dplyr)
-library(tsibble)
-
-weather <- nycflights13::weather %>% 
-  select(origin, time_hour, temp, humid, precip)
-
-
-weather_tsbl <- as_tsibble(weather, key = origin, index = time_hour)
